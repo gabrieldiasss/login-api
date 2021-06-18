bbab52db2456d0e693b4107f71c6b1fc7fc0a0c4
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2021-06-18 13:52:19 -0300

    change validation

--- src/Validations/registerValidation.js
+++ src/Validations/registerValidation.js
@@ -1,9 +1,9 @@
 const yup = require("yup")
 
 const loginSchema = yup.object({
-    name: yup.string().max(15, "Seu nome de usuário deve ter menos que 15 caracteres").required("O nome de usuário é obrigatório!") ,
+    name: yup.string().required("O nome de usuário é obrigatório!").max(15, "Seu nome de usuário deve ter menos que 15 caracteres") ,
     email: yup.string().email("Formato do email está errado.").required("O Email é obrigatório!"),
-    password: yup.string().min(6, "Sua senha deve ter no mínimo 6 caracteres").required("A senha é obrigatória!")
+    password: yup.string().required("A senha é obrigatória!").min(6, "Sua senha deve ter no mínimo 6 caracteres")
 });
 
 module.exports = loginSchema;

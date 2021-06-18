42fa39411ccc5c04a358bd25d8b71950403b4a32
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2021-06-18 13:42:54 -0300

    chage validation error message

--- src/Validations/loginValidation.js
+++ src/Validations/loginValidation.js
@@ -1,7 +1,7 @@
 const yup = require("yup")
 
 const loginSchema = yup.object({
-    email: yup.string().email("É necessário ter @ no email.").required("Campo email é requirido"),
+    email: yup.string().email("Formato do email está errado.").required("Campo email é requirido"),
     password: yup.string().required("Campo senha é requirido!")
 });
 
--- src/Validations/registerValidation.js
+++ src/Validations/registerValidation.js
@@ -1,8 +1,8 @@
 const yup = require("yup")
 
 const loginSchema = yup.object({
-    name: yup.string().required("O nome é obrigatório") ,
-    email: yup.string().email("É necessário ter @ no email.").required("O email é requirido"),
+    name: yup.string().max(15, "Seu nome de usuário deve ter menos que 15 caracteres").required("O nome de usuário é obrigatório!") ,
+    email: yup.string().email("Formato do email está errado.").required("O Email é obrigatório!"),
     password: yup.string().min(6, "Sua senha deve ter no mínimo 6 caracteres").required("A senha é obrigatória!")
 });
 

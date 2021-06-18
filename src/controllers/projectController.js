02248180adf0b644825d87241c23a0ef5956e8a9
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2021-06-18 12:51:57 -0300

    chage authenticated  message

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -6,7 +6,7 @@ const authMiddleware = require("../middlewares/auth")
 router.use(authMiddleware)
 
 router.get("/", (req, res) => {
-    res.json("Você está autenticado!!!!" )
+    res.json("Você acessou está página, pq vc está autenticado!!!!" )
 })
 
 module.exports = router

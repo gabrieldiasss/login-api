1271b0acefa67a04890c6812bb8d4c52d849db1a
Gabriel  Dias <gabrieldiascorrea7@gmail.com>
2021-11-06 01:44:09 -0300

    back all normal

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -37,7 +37,17 @@ router.get("/listuser", async(req, res) => {
 router.post("/register", validation(registerValidation), async(req, res) => {
     
     try {
-        const { email } = req.body
+        const { name, email } = req.body
+
+        const userExists = await User.findOne({ name, email })
+
+        if(userExists)
+            return res.status(400).json({ error: "Essa conta já existe." })
+
+        const nameExists = await User.findOne({ name })
+
+        if(nameExists)
+            return res.status(400).json({ error: "Esse nome já está em uso." })
 
         const emailExists = await User.findOne({ email })
 
